20937a21cd3bf7922b53d49aa0b63be881ea7cb1
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2024-03-18 10:43:19 +0000

    Remove legacy files

--- HyPER/data/__init__.py
+++ HyPER/data/__init__.py
@@ -1,9 +1,7 @@
 from .dataset import GraphDataset
 from .datamodule import HyPERDataModule
-from .interfaceROOT import root2dataframe
 
 __all__ = [
     'GraphDataset',
-    'HyPERDataModule',
-    'root2dataframe'
+    'HyPERDataModule'
 ]
--- HyPER/utils/__init__.py
+++ HyPER/utils/__init__.py
@@ -1,7 +1,5 @@
-from .h5toROOT import h5toROOT
 from .settings import Settings
 
 __all__ = [
-    'h5toROOT',
     'Settings'
 ]
