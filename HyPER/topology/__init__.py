4abd308173211c215a37c5a997f261d74e2139a4
tzuhanchang <han.tzu.zhang@gmail.com>
2025-05-06 15:15:41 +0100

    Reset docs update

--- HyPER/topology/__init__.py
+++ HyPER/topology/__init__.py
@@ -1,7 +1,9 @@
 from .ttbar import ttbar_allhad
+from .ttbar import ttbar_single_lep
 from .FourTop import FourTop_allhad
 
 __all__ = [
     'ttbar_allhad',
+    'ttbar_single_lep',
     'FourTop_allhad'
 ]
